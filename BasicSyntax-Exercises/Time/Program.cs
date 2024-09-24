47528224c5e9b8b7041913064b4a1c484515538d
Ivaylo <ivailomn216@gmail.com>
2024-09-24 18:03:20 +0300

    initial commit

---
+++ BasicSyntax-Exercises/Time/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int minutes = int.Parse(Console.ReadLine());
+
+        // Пресмятане на часове и минути
+        int hours = minutes / 60;
+        int remainingMinutes = minutes % 60;
+
+        // Форматиране и принтиране в HR:MM формат
+        Console.WriteLine($"{hours:D2}:{remainingMinutes:D2}");
+    }
+}
