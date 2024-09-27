cf52025bdc154501a2811008ea2f3bb9dd42dd7a
Ivaylo <ivailomn216@gmail.com>
2024-09-27 15:52:17 +0300

    initial commit

---
+++ ConditionalStatements-Exercises/07.Speed/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _07.Speed
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double speed = double.Parse(Console.ReadLine());
+
+            if (speed <= 10)
+            {
+                Console.WriteLine("slow");
+            }
+            else if (speed > 10 && speed <= 60)
+            {
+                Console.WriteLine("average");
+            }
+            else if (speed > 60 && speed <= 120)
+            {
+                Console.WriteLine("fast");
+            }
+            else if (speed > 120 && speed <= 160)
+            {
+                Console.WriteLine("super-fast");
+            }
+            else if (speed > 160)
+            {
+                Console.WriteLine("turbo-fast");
+            }
+        }
+    }
+}
