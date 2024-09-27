3a498e727bcecec5fd8ffffa92ea4962dc0b9e63
Ivaylo <ivailomn216@gmail.com>
2024-09-27 12:58:10 +0300

    initial commit

---
+++ ConditionalStatements-Exercises/02.LargerNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _02.LargerNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOne = int.Parse(Console.ReadLine());
+            int numberTwo = int.Parse(Console.ReadLine());
+
+            if (numberOne > numberTwo)
+            {
+                Console.WriteLine(numberOne);
+            }
+            else
+            {
+                Console.WriteLine(numberTwo);
+            }
+        }
+    }
+}
---
+++ ConditionalStatements-Exercises/04.FaceOfFigures/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _04.FaceOfFigures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string figure = Console.ReadLine();
+
+            if (figure == "square")
+            {
+                double lengthOfSide = double.Parse(Console.ReadLine());
+                Console.WriteLine($"{lengthOfSide * lengthOfSide:F2}");
+            }
+            else if (figure == "rectangle")
+            {
+                double sideA = double.Parse(Console.ReadLine());
+                double sideB= double.Parse(Console.ReadLine());
+                Console.WriteLine($"{sideA * sideB:F2}");
+            }
+            else if (figure == "circle")
+            {
+                double radius = double.Parse(Console.ReadLine());
+                double area = Math.PI * Math.Pow(radius, 2);
+                Console.WriteLine($"{area:F2}");
+            }
+            else if (figure == "triangle")
+            {
+                double lengthOfSide = double.Parse(Console.ReadLine());
+                double lengthOHeight = double.Parse(Console.ReadLine());
+                double area = 0.5 * lengthOfSide * lengthOHeight;
+                Console.WriteLine($"{area:F2}");
+            }
+        }
+    }
+}
