a36fa9732c8d8bcb3367660b27a2422728d68e01
Ivaylo <ivailomn216@gmail.com>
2024-09-27 17:47:27 +0300

    initial commit

---
+++ ConditionalStatements-Exercises/09.AddressByAgeAndGender/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _09.AddressByAgeAndGender
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = int.Parse(Console.ReadLine());
+            string gender = Console.ReadLine();
+
+            if (gender == "m")
+            {
+                if (age >= 16)
+                {
+                    Console.WriteLine("Mr.");
+                }
+                else
+                {
+                    Console.WriteLine("Master");
+                }
+            }
+            else if (gender == "f")
+            {
+                if (age >= 16)
+                {
+                    Console.WriteLine("Ms.");
+                }
+                else
+                {
+                    Console.WriteLine("Miss");
+                }
+            }
+        }
+    }
+}
