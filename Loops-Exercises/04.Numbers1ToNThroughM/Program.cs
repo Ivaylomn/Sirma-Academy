091255f4a544ff2eeebdcdda671b67f3461fe44f
Ivaylo <ivailomn216@gmail.com>
2024-10-01 19:00:15 +0300

    initial commit

---
+++ Loops-Exercises/04.Numbers1ToNThroughM/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _04.Numbers1ToNThroughM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int step = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < n; i += step)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ Loops-Exercises/09.SumOfEvenNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _09.SumOfEvenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    sum += 2 * i;
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
