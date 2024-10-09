2e00dc612b1fce3f8985d18e88667cfc198fddac
Ivaylo <ivailomn216@gmail.com>
2024-10-09 17:44:05 +0300

    initial commit

---
+++ Classes-Exercises/03.BankAccount/BankAccount.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace _03.BankAccount
+{
+    internal class BankAccount
+    {
+        private int id;
+        private double balance;
+        private static double interestRate = 0.02;
+
+        public int Id { get => id; set => id = value; }
+
+        public static void SetInterestRate(double interest)
+        {
+            interestRate = interest;
+        }
+
+        public double GetInterest(int years)
+        {
+            return this.balance * interestRate * years;
+        }
+
+        public void Deposit(double amount) 
+        {
+            this.balance += amount;
+        }
+    }
+}
---
+++ Classes-Exercises/03.BankAccount/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace _03.BankAccount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] command = Console.ReadLine().Split().ToArray();
+
+            List<BankAccount> bankAccounts = new List<BankAccount>();
+            int countId = 1;
+            while (command[0] != "End")
+            {
+                if (command[0] == "Create")
+                {
+                    BankAccount bankAccount = new BankAccount();
+                    bankAccount.Id = countId;
+                    Console.WriteLine($"Account ID{countId} created");
+                    countId++;
+                    bankAccounts.Add(bankAccount);
+                }
+                else if (command[0] == "Deposit")
+                {
+                    int id = int.Parse(command[1]);
+                    double amount = double.Parse(command[2]);
+                    BankAccount account = bankAccounts.Where(x => x.Id == id).FirstOrDefault();
+                    if (account != null)
+                    {
+                        account.Deposit(amount);
+                        Console.WriteLine($"Deposited {amount} to ID{id}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Account does not exist.");
+                    }
+                }
+                else if (command[0] == "SetInterest")
+                {
+                    double newInterest = double.Parse(command[1]);
+                    BankAccount.SetInterestRate(newInterest);
+                }
+                else if (command[0] == "GetInterest")
+                {
+                    int id = int.Parse(command[1]);
+                    int years = int.Parse(command[2]);
+                    BankAccount account = bankAccounts.Where(x => x.Id == id).FirstOrDefault();
+                    if (account != null)
+                    {
+                        double interestRate = account.GetInterest(years);
+                        Console.WriteLine($"{interestRate:F2}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Account does not exist.");
+                    }
+                    
+                }
+
+                command = Console.ReadLine().Split().ToArray();
+            }
+        }
+    }
+}
