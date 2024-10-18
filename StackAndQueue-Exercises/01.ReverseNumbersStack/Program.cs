e0d15a253de96e4190cdd7ef653c95f17c7ddd3f
Ivaylo <ivailomn216@gmail.com>
2024-10-18 19:55:31 +0300

    initial commit

---
+++ StackAndQueue-Exercises/01.ReverseNumbersStack/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _01.ReverseNumbersStack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> numbers = new Stack<int>(Console.ReadLine().Split().Select(int.Parse));
+
+            foreach (var item in numbers)
+            {
+                Console.Write(item + " ");
+            }
+
+        }
+    }
+}
