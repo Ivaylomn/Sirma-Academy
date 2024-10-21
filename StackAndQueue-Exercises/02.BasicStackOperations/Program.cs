ff41fae1dc2ee5e3ddc12c7c3396ae2b003048a2
Ivaylo <ivailomn216@gmail.com>
2024-10-21 16:39:57 +0300

    initial commit

---
+++ StackAndQueue-Exercises/02.BasicStackOperations/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace _02.BasicStackOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] commands = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int n = commands[0];
+            int numbersToPopFromStack = commands[1];
+            int x = commands[2];
+
+            Stack<int> stack = new Stack<int>();
+            for (int i = 0; i < n; i++)
+            {
+                stack.Push(numbers[i]);
+            }
+           
+            for (int i = 0; i < numbersToPopFromStack; i++)
+            {
+                if (stack.Count > 0)
+                {
+                    stack.Pop();
+                }
+            }
+
+            bool isContainElement = false;
+            if (stack.Contains(x))
+            {
+                isContainElement = true;
+            }
+
+            int smallestElement = int.MaxValue;
+            foreach (var item in stack)
+            {
+
+                if (item < smallestElement)
+                {
+                    smallestElement = item;
+                }
+            }
+
+            if (stack.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else if (isContainElement)
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine(smallestElement);
+            }
+        }
+    }
+}
---
+++ StackAndQueue-Exercises/03.Browser/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _03.Browser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //TODO: прегледай задачата на ново!!!
+            Stack<string> history = new Stack<string>();
+            string currentURL = null;
+
+            string command = Console.ReadLine();
+
+            while (command != "Home")
+            {
+                if (command == "back")
+                {
+                    if (history.Count > 0)
+                    {
+                        currentURL = history.Pop();
+                    }
+                    else
+                    {
+                        Console.WriteLine("no previous URLs");
+                        command = Console.ReadLine();
+                        continue;
+                    }
+                }
+                else
+                {
+                    if (currentURL != null)
+                    {
+                        history.Push(currentURL);
+                    }
+                    currentURL = command;
+                }
+
+                Console.WriteLine(currentURL);
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
