c05feb74cd32196ece8f9d0308ee556b6dbbf197
Ivaylo <ivailomn216@gmail.com>
2024-11-01 15:19:38 +0200

    initial commit

---
+++ StackAndQueue-Exercises/04.SimpleCalculator/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+class SimpleCalculator
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        string[] tokens = input.Split(' ');
+        Stack<int> stack = new Stack<int>();
+
+        int i = 0;
+        while (i < tokens.Length)
+        {
+            if (int.TryParse(tokens[i], out int number))
+            {
+                stack.Push(number);
+            }
+            else
+            {
+                string operatorToken = tokens[i];
+                i++;
+                int nextNumber = int.Parse(tokens[i]);
+
+                if (operatorToken == "+")
+                {
+                    stack.Push(nextNumber);
+                }
+                else if (operatorToken == "-")
+                {
+                    stack.Push(-nextNumber);
+                }
+            }
+            i++;
+        }
+
+        int result = 0;
+        while (stack.Count > 0)
+        {
+            result += stack.Pop();
+        }
+
+        Console.WriteLine(result);
+    }
+}
