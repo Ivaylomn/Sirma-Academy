051efef2b773af05e8c839f0041dcf8a32d615a6
Ivaylo <ivailomn216@gmail.com>
2024-11-18 18:00:26 +0200

    initial commit

---
+++ SetsAndMaps-Exercises/03.CountSymbols/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _03.CountSymbols
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char currentElement = text[i];
+                if (!dict.ContainsKey(currentElement))
+                {
+                    dict.Add(currentElement, 0);
+                }
+
+                dict[currentElement]++;
+            }
+
+            foreach (var item in dict.OrderBy(x => x.Key))
+            {
+                Console.WriteLine($"{item.Key}: {item.Value}");
+            }
+        }
+    }
+}
---
+++ SetsAndMaps-Exercises/04.Phonebook/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace _04.Phonebook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Dictionary<string, string> dict = new Dictionary<string, string>();
+            while (input != "search")
+            {
+                string[] inputArgs = input.Split("-");
+                string name = inputArgs[0];
+                string number = inputArgs[1];
+
+                dict[name] = number;
+
+                input = Console.ReadLine();
+            }
+
+            input = Console.ReadLine();
+            while (input != "stop")
+            {
+                if (input.Contains('-'))
+                {
+                    string[] inputArgs = input.Split("-");
+                    string name = inputArgs[0];
+                    string number = inputArgs[1];
+
+                    dict[name] = number;
+                }
+                else
+                {
+                    if (dict.ContainsKey(input))
+                    {
+                        Console.WriteLine($"{input} -> {dict[input]}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Contact {input} not found.");
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
