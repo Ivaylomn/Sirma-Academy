b33fa4a0f4c6186caa8a1950db4d1a3d36e7ba90
Ivaylo <ivailomn216@gmail.com>
2024-11-18 12:24:49 +0200

    initial commit

---
+++ SetsAndMaps-Exercises/01.CountRealNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _01.CountRealNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] input = Console.ReadLine().Split().Select(double.Parse).ToArray();
+            Dictionary<double, int> result = new Dictionary<double, int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                double currentElement = input[i];
+                if (!result.ContainsKey(currentElement))
+                {
+                    result.Add(currentElement, 0);
+                }
+
+                result[currentElement]++;
+            }
+
+            foreach (var item in result)
+            {
+                Console.WriteLine($"{item.Key:F1} -> {item.Value}");
+            }
+        }
+    }
+}
---
+++ SetsAndMaps-Exercises/02.AverageStudentsGrades/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _02.AverageStudentsGrades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            SortedDictionary<string, List<double>> studentInformation = new SortedDictionary<string, List<double>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] nameAndGrade = Console.ReadLine().Split();
+                string name = nameAndGrade[0];
+                double grade = double.Parse(nameAndGrade[1]);
+
+                if (!studentInformation.ContainsKey(name))
+                {
+                    studentInformation.Add(name, new List<double>());
+                }
+
+                studentInformation[name].Add(grade);
+            }
+
+            foreach (var item in studentInformation)
+            {
+                Console.Write($"{item.Key} -> ");
+
+                //string.Join(" ", item.Value.Select(g => g.ToString("F2"))
+                foreach (var grade in item.Value)
+                {
+                    Console.Write($"{grade:f2} ");
+                }
+                                          //Or We can use method average: item.Value.Average()
+                Console.WriteLine($"(avg: {item.Value.Sum() / item.Value.Count():F2})");
+
+            }
+        }
+    }
+}
