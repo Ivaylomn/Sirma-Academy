76d3da7133319b87ae00f77c38e47fc29fde932b
Ivaylo <ivailomn216@gmail.com>
2024-11-25 17:48:11 +0200

    initial commit

---
+++ StreamsAndFiles/01.WordLengths/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _01.WordLengths
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputPath = @"D:\Ivaylo\Sirma-Academy\Educational-Resources\OOP\StreamsAndFiles\01.WordLengths\Input.text";
+            string outputPath = @"D:\Ivaylo\Sirma-Academy\Educational-Resources\OOP\StreamsAndFiles\01.WordLengths\Output.txt";
+
+            using StreamReader reader = new StreamReader(inputPath);
+            string[] text = reader.ReadLine().Split().ToArray();
+
+            List<int> numbers = new List<int>();
+
+            while (reader.EndOfStream)
+            {
+                foreach (var word in text)
+                {
+                    numbers.Add(word.Length);
+                }
+            }
+
+            using StreamWriter writer = new StreamWriter(outputPath);
+            writer.Write(numbers.ToArray());
+        }
+    }
+}
