047e555d6828eb2b195af58719477641160fcce4
Ivaylo <ivailomn216@gmail.com>
2024-12-03 11:28:41 +0200

    initial commit

--- StreamsAndFiles/01.WordLengths/Program.cs
+++ StreamsAndFiles/01.WordLengths/Program.cs
@@ -4,24 +4,26 @@
     {
         static void Main(string[] args)
         {
-            string inputPath = @"D:\Ivaylo\Sirma-Academy\Educational-Resources\OOP\StreamsAndFiles\01.WordLengths\Input.text";
+            string inputPath = @"D:\Ivaylo\Sirma-Academy\Educational-Resources\OOP\StreamsAndFiles\01.WordLengths\Input.txt";
             string outputPath = @"D:\Ivaylo\Sirma-Academy\Educational-Resources\OOP\StreamsAndFiles\01.WordLengths\Output.txt";
 
             using StreamReader reader = new StreamReader(inputPath);
-            string[] text = reader.ReadLine().Split().ToArray();
+            
 
             List<int> numbers = new List<int>();
-
-            while (reader.EndOfStream)
+            while (!reader.EndOfStream)
             {
+                string line = reader.ReadLine();
+                string[] text = line.Split();
                 foreach (var word in text)
                 {
                     numbers.Add(word.Length);
                 }
+
             }
 
             using StreamWriter writer = new StreamWriter(outputPath);
-            writer.Write(numbers.ToArray());
+            writer.Write(string.Join(", ", numbers));
         }
     }
 }
